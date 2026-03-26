4a783eb6c71f0a4a38f3cf409dd2c7c78e5e6b61
Bryanrobu <1211898@student.roc-nijmegen.nl>
2026-03-26 11:40:15 +0100

    een startscherm aangemaakt waar je een training sessie kan starten

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -80,7 +80,7 @@ namespace WinFormsApp1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(803, 450);
             Controls.Add(label1);
             Controls.Add(button3);
             Controls.Add(button2);
---
+++ WinFormsApp1/StartScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            button1.Text = "Start Game";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
